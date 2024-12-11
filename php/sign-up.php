02f13e615188985cdf4b2b8aa4a3a6bec41727e2
Troyoyo <134513646+Troyhenryyy@users.noreply.github.com>
2024-12-11 15:25:45 +0800

    registration

---
+++ php/sign-up.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title> Responsive Registration Form | CodingLab </title>
+  <link rel="stylesheet" href="styletest.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+ 
+
+   <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+   
+<section class="login-page">
+
+  <div class="login-parts">
+  
+    <div class="login-left">
+      <img src="big-logo.png" alt="big logo" class="login-logo">
+    </div>
+    <div class="login-left thicc">
+      <div class="login-box">
+        <p class="login-header">Register</p>
+        <p class="login-labels">Email</p>
+        <input type="email" placeholder="email@example.com">
+        <p class="login-labels">Password</p>
+        <input type="password" placeholder="enter your password">
+        <p class="login-labels">Confirm Password</p>
+        <input type="password" placeholder="re-enter your password">
+        <p class="login-labels">Phone Number</p>
+        <input type="password" placeholder="enter your number">
+        <button class="login-button">Login</button>
+        <a href="#forgot" class="login-forgot">Forgot Password?</a>
+      
+        <p>Don't have an account? <a href="#register">Register here</a></p>
+      </div>
+    </div>
+  
+
+  </div>
+
+  <div class="login-parts login-back-img">
+    
+  </div>
+
+
+
+</section>
+
+
+</body>
+</html>
+
+
+
+
+</body>
+<script>
+    window.onload = function () {
+  "use strict";
+};
+
+function handleSelectChange() {
+      const select = document.getElementById("shoppingPlatform");
+      const textInput = document.querySelector(".text-input");
+
+      // Show or hide the textbox based on the selected value
+      if (select.value === "4") {
+        textInput.style.display = "block";
+      } else {
+        textInput.style.display = "none";
+      }
+    }
+
+</script>
+</html>
